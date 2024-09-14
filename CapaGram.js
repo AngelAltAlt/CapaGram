3b53a943cd9fb6bfabfcb447be139e325ef6acbf
Agsgt2 <angelthefourth20242026@gmail.com>
2024-09-14 16:12:44 -0600

    Create CapaGram.js

---
+++ CapaGram.js
@@ -0,0 +1,21 @@
+//Required Custom Extensions//
+import * from "./ext/CapaGram-Basic.js"
+import * from "./ext/Console-Basic.js"
+
+//Required Script Extensions//
+function unlock(x){
+  if (x == true){
+    return 0
+  } else {
+    error("This has not been unlocked")
+  }
+}
+
+//Scripts//
+
+export function start(){
+  let Unlocked = true
+  print("Starting CapaGram...")
+}
+
+//More comming soon...
