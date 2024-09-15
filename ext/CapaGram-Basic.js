cfa76f5e23668c9c6400cf7840c3d220d885238c
Agsgt2 <angelthefourth20242026@gmail.com>
2024-09-14 21:55:19 -0600

    Create CapaGram-Basic.js

---
+++ ext/CapaGram-Basic.js
@@ -0,0 +1,10 @@
+//Needed custom extensions//
+import * from "Console-Basic.js"
+
+//Scripts//
+export function varOutBasic(x){
+   return x
+   printc("Value eleminated and outed sucuesfully!")
+}
+
+//FREE TRIAL MODE//
